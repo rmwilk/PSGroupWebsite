dfd0a6eaccf6ae293f47cb8d1145b49e1a3a549f
rmwilk <rwilk@umich.edu>
2019-12-16 12:05:02 -0500

    Added priotiy drop down and other style blends

--- WebContent/scripts/table.js
+++ WebContent/scripts/table.js
@@ -8,6 +8,7 @@ function appendRow() {
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
 
     //Declare ID for each row
     row.id = "row1" + i;
@@ -15,14 +16,15 @@ function appendRow() {
 
     //Create checkbox
     var checkBox = '<input type="checkbox" id="cBox" onclick="crossOut(\'' + row.id + '\')" value="no">';
-
+    
     //Create delete
     var deleteButton = '<button type="button" onclick="deleteRow(this)">Delete</button>';
 
     cell1.innerHTML = checkBox;
     cell2.innerHTML = document.getElementById("activity").value;
     cell3.innerHTML = document.getElementById("description").value;
-    cell4.innerHTML = deleteButton;
+    cell4.innerHTML = document.getElementById("priority").value;
+    cell5.innerHTML = deleteButton;
 }
 
 function deleteRow(r) {
